4549438a5906d0477ce78e764c1f216ffbe3563a
Yashank <yashank7696@gmail.com>
2023-01-18 19:00:11 +0530

    chore: adding redux and redux-saga

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import {Provider} from 'react-redux';
+import createSagaMiddleWare from 'redux-saga';
+import {configureStore} from '@reduxjs/toolkit';
+
+const saga = createSagaMiddleWare();
+const store = configureStore({
+  reducer: {},
+  middleware: [saga],
+}); 
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
